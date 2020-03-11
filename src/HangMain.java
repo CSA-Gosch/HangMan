392584cb22babd0e0689297568b87f7f077160ae
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2020-03-11 11:10:16 -0500

    bruh

---
+++ src/HangMain.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HangMain {
+
+    public static void main(String[] args)
+    {
+        String[] words = {"Tool", "INK", "Metallica"};
+        Scanner input = new Scanner(System.in);
+
+
+
+        for(int i = 0; i <= words.length; i++)
+        {
+            words[0] =
+        }
+    }
+
+}
