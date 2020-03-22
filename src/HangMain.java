85939103191761bb704b3e993ade4936ef8ebbf2
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2020-03-21 21:26:04 -0500

    bruh

--- src/HangMain.java
+++ src/HangMain.java
@@ -6,52 +6,85 @@ public class HangMain {
     {
         Graphics.Graphics();
 
-        String[] words = {"Tool", "INK", "Metallica"};
+        String[] words = {"tool", "nirvana", "metallica"};
 
         Scanner input = new Scanner(System.in);
 
         int random;
-        int limit = 0;
-        String userEntry;
         random = ((int) (Math.random() * words.length));
-        String randomString = words[random];
+        String word = words[random];
 
-        while
+        int limit = 0;
 
-        for(int i = 0; i < randomString.length(); i++)
-        {
-            while (limit < 7)
+        String hiddenWord = "";
+
+        for(int j = 1; j <= word.length(); j++)
+        { hiddenWord += "-"; }
+
+        while(limit < 6 && hiddenWord.contains("-")) {
+            System.out.println("Guess a letter in the word >>> ");
+            System.out.println(hiddenWord);
+            String userEntry = input.next();
+
+            String replace = "";
+
+            for (int i = 0; i < word.length(); i++)
             {
-                System.out.print("Guess a letter in the word >>> ");
-                userEntry = input.nextLine();
-                if (limit == 1)
-                {
-                    Graphics.GraphicsTwo();
+                if (word.charAt(i) == userEntry.charAt(0)) {
+                    replace += userEntry.charAt(0);
                 }
-                if (limit == 2)
+                else if (hiddenWord.charAt(i) != '-')
                 {
-                    Graphics.GraphicsThree();
+                    replace += word.charAt(i);
                 }
-                if (limit == 3)
-                {
-                    Graphics.GraphicsFour();
-                }
-                if (limit == 4)
-                {
-                    Graphics.GraphicsFive();
-                }
-                if (limit == 5)
-                {
-                    Graphics.GraphicsSix();
-                }
-                else if (limit == 5)
-                {
-                    Graphics.GraphicsSeven();
-                    System.out.println("Game Over, the word was " + randomString);
+                else
+                    {
+                    replace += "-";
                 }
             }
+            if(hiddenWord.equals(replace))
+            {
+                limit++;
+            }
+            else
+            {
+                hiddenWord = replace;
+            }
 
+            if(hiddenWord.equals(word))
+            {
+                System.out.print("You won. You're so cool, " +
+                        "can I have you're autograph? >>> ");
+                String leaderboard = input.nextLine();
+            }
         }
+
+            if (limit == 1) {
+                Graphics.GraphicsTwo();
+                System.out.println("WRONG!");
+            }
+            if (limit == 2) {
+                Graphics.GraphicsThree();
+                System.out.println("WRONG!");
+            }
+            if (limit == 3) {
+                Graphics.GraphicsFour();
+                System.out.println("WRONG!");
+            }
+            if (limit == 4) {
+                Graphics.GraphicsFive();
+                System.out.println("WRONG!");
+            }
+            if (limit == 5) {
+                Graphics.GraphicsSix();
+                System.out.println("WRONG!");
+            }
+            else if (limit == 6) {
+                Graphics.GraphicsSeven();
+                System.out.println("You failed." +
+                        " Do not pass go... do not collect $200. " +
+                        "The word was: " + word);
+            }
 
     }
 
